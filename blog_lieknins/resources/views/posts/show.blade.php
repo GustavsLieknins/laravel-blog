f5b357db3b23a3a02be2efda627bb69e9cd493d2
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-04-25 11:26:37 +0300

    Added

---
+++ blog_lieknins/resources/views/posts/show.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Showing</h1>
+    <ul>
+        <li><a href="/">{{ $post->title }}</a></li>
+    </ul>
+</body>
+</html>
